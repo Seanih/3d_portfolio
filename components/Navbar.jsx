f032bbce2e2f48096b2f04dbb36a7682df0dd98f
Sean Anih <sean.anih@gmail.com>
2023-03-06 20:59:44 -0500

    structured ui components & started Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../public/assets';
+import Image from 'next/image';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	const [active, setActive] = useState('');
+	return (
+		<nav
+			className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+		>
+			<div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+				<Link
+					href={'/'}
+					className='flex items-center gap-2'
+					onClick={() => {
+						setActive('');
+						window.scrollTo(0, 0);
+					}}
+				>
+					<Image src={logo} alt='logo' className='w-9 h-9 object-contain' />
+					<p className='text-white text-[18px] font-bold cursor-pointer'>
+						Sean <span className='sm:block hidden'>| FullChain Solutions</span>
+					</p>
+				</Link>
+				<p>blah blah</p>
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
